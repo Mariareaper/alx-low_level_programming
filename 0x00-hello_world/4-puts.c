6ba92c0fd35fb5d4f902cff58722fd3085a2ca28
Mariareaper <marymuwanja@gmail.com>
2023-07-14 12:04:15 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stio.h
+/**
+ * main - A program that uses the puts function to print a new line
+ * retur: 0 (success)
+ */
+int main(void)
+}
+puts("\"Programming is like building a multilingual puzzle")
+return (0);
+}
